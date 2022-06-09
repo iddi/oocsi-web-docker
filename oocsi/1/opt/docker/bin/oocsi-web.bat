9d7b4c8029812781ad7a194e8641cf8197d87c89
matsfunk <mathias.funk@gmail.com>
2022-06-09 20:12:44 +0200

    - updated server to v0.3.37

--- oocsi/1/opt/docker/bin/oocsi-web.bat
+++ oocsi/1/opt/docker/bin/oocsi-web.bat
@@ -41,7 +41,7 @@ set _JAVA_PARAMS=
 set _APP_ARGS=
 
 set "APP_CLASSPATH="
-set "APP_MAIN_CLASS=-jar "%APP_LIB_DIR%\IndustrialDesign.oocsi-web-0.3.27-launcher.jar""
+set "APP_MAIN_CLASS=-jar "%APP_LIB_DIR%\IndustrialDesign.oocsi-web-0.3.37-launcher.jar""
 set "SCRIPT_CONF_FILE=%APP_HOME%\conf\application.ini"
 
 rem Bundled JRE has priority over standard environment variables
